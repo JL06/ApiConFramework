4348a2e47e03a76dcd533d1cd2a8acd6a9c4c48e
Juan Hernandez <jl_hl@hotmail.com>
2022-01-27 12:59:21 -0600

    Creacion de framework

--- Sources/ApiConFramework/ApiConFramework.swift
+++ Sources/ApiConFramework/ApiConFramework.swift
@@ -1,6 +1,29 @@
-public struct ApiConFramework {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+public class ApiConFramework {
+    private var urlTexto: String
+    var datosRecientes: Data?
+    
+    public init(url: String) {
+        self.urlTexto = url
     }
+    
+    public func obtenerRecurso(finalizo: @escaping (Data?) -> ()) {
+        let urlSes = URLSession(configuration: URLSessionConfiguration.default)
+        let urlS = URL(string: self.urlTexto)
+        
+        urlSes.dataTask(with: urlS!) { info, response, error in
+            DispatchQueue.main.async {
+                self.datosRecientes = info
+            }
+            finalizo(info)
+        }.resume()
+    }
+    
+    public func obtenerRecurso(de url: String, finalizo: @escaping (Data?) -> ()) {
+        self.urlTexto = url
+        self.obtenerRecurso(finalizo: finalizo)
+    }
+
+    
 }
--- Tests/ApiConFrameworkTests/ApiConFrameworkTests.swift
+++ Tests/ApiConFrameworkTests/ApiConFrameworkTests.swift
@@ -2,10 +2,33 @@ import XCTest
 @testable import ApiConFramework
 
 final class ApiConFrameworkTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(ApiConFramework().text, "Hello, World!")
+    func testSession () {
+        let obj: ApiConFramework = ApiConFramework(url: "https://api.themoviedb.org/3/movie/550?api_key=05e3d00ae13d42a1dda51e75a27fcc49&language=en-US")
+        
+        let exp = self.expectation(description: "sesion")
+        obj.obtenerRecurso { info in
+            if info != nil {
+                exp.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 3, handler: nil)
+        
+        //let texto = String(decoding: obj.datosRecientes!, as: UTF8.self)
+        //print("Resultado valor data: \(texto)")
+    }
+    
+    func testSessionString () {
+        let obj: ApiConFramework = ApiConFramework(url: "https://api.themoviedb.org/3/movie/550?api_key=05e3d00ae13d42a1dda51e75a27fcc49&language=en-US")
+        
+        let exp = self.expectation(description: "sesion")
+        obj.obtenerRecurso(de: "https://api.themoviedb.org/3/movie/551?api_key=05e3d00ae13d42a1dda51e75a27fcc49&language=en-US") { info in
+            if info != nil {
+                exp.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 3, handler: nil)
+        
+        //let texto = String(decoding: obj.datosRecientes!, as: UTF8.self)
+        //print("Resultado valor data: \(texto)")
     }
 }
